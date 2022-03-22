62b8c48600988ec30fed7be272e552a87c333270
Han <59479453+recta-rrae@users.noreply.github.com>
2022-03-22 17:07:00 +0700

    Changes on tailwind.config.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main className="mt-10">
-        <h1>Main Page</h1>
+        <h1 className="mt-10 mx-auto my-auto">HELLLOOO BITCHESSS</h1>
       </main>
     </div>
   );
